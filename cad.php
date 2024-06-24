f25f924984476f5c0482a0be829c12a1600d55bb
CollinTheDev <collin.entringer@gmail.com>
2024-06-23 19:27:41 -0500

    Delete cad.php

--- cad.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url = 'https://demo.communitycad.app/';
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
-echo $response;
