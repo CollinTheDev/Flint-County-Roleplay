8a65e1ebbbdb3738dbf28448474292e6a480a391
CollinTheDev <collin.entringer@gmail.com>
2024-06-23 19:21:50 -0500

    Create cad.php

---
+++ cad.php
@@ -0,0 +1,8 @@
+<?php
+$url = 'https://example.com';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
